c4253b38fa6be14b6603090f5819d266c3fb738d
Nayaabkhan Khan <nayaab@hey.com>
2022-01-27 09:06:52 +0530

    Add alt prop to images

--- library/components/controls/Image.tsx
+++ library/components/controls/Image.tsx
@@ -2,8 +2,11 @@ import { Roundable, rounded } from '@modifiers/Roundable'
 import { Sizeable, size } from '@modifiers/Sizeable'
 import NextImage from 'next/image'
 
-export default function Image(path: string): Sizeable & Roundable {
-  const element = <NextImage src={path} />
+export default function Image(
+  path: string,
+  alt?: string,
+): Sizeable & Roundable {
+  const element = <NextImage src={path} alt={alt} />
 
   return {
     ...element,
